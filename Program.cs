05448c24a0dbe53c6f8a3c504a9da967001ef846
Valorando <vadimsokil.dnipro@gmail.com>
2023-08-30 22:57:46 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*Создайте в SSMS таблицу инетренат магазина, которая будет хранить в себе поля по продукту. 
+Наполните 5 продуктами. После чего установите отсоединенный режим подключения к данной БД и таблице. 
+Добавьте 5 продуктов. Измените продукты которые уже были в бд. Изменить 5 продуктов(по одному свойству)
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_31_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
